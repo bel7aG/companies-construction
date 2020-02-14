5b4799c79ccb4a15d3fbf4e7adc7479988bce6ca
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2020-02-15 00:17:04 +0100

    gatsby-node configuration (Absolute path & Turn off source maps

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,16 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  })
+
+  if (stage === "build-javascript") {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import SEO from "../components/seo"
-import Header from "../components/Header"
+import { Header } from "components"
 
 const IndexPage = () => (
   <div>
